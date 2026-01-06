3e7bb12a4b7e3195ba874286e94aa3f564cabab5
Kos-qa <konstantin.ivanov@bluebricks.co>
2026-01-06 14:25:34 +0200

    Add VPC and public subnet resources in Terraform

---
+++ test-source/multi-lvl-tf/modules/network/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.env_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env_name}-public-subnet"
+  }
+}
